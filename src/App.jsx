79ee21a09652342797ca18482592969357413000
doston <hasanovdostonbek870@gmail.com>
2023-01-26 23:00:50 +0500

    add 404 page

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,41 @@ import { Item } from "./components/Item";
 import { Header } from "./components/Headers";
 import { useEffect, useState, useRef } from "react";
 import search from ".././src/assets/img/search.svg";
+import loading from ".././src/assets/img/loading1.gif";
+import error from ".././src/assets/img/404.png";
 
 function App() {
- 
-
   let [country, setCountry] = useState({
     loading: true,
     data: [],
     err: "",
   });
+
+  useEffect(() => {
+    setCountry({ loading: true, data: [], err: "" });
+    fetch("https://restcountries.com/v3.1/all")
+      .then((res) => res.json())
+      .then((data) => {
+        if (data) {
+          setCountry({ loading: false, data: data, err: "" });
+        }
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  let changeSelect = (evt) => {
+    function allRegion() {
+      fetch(`https://restcountries.com/v3.1/region/${evt.target.value}`)
+        .then((res) => res.json())
+        .then((data) => {
+          if (data) {
+            setCountry({ loading: false, data: data, err: "" });
+          }
+        });
+    }
+
+    allRegion();
+  };
 
   let inputValue = useRef();
   let changeForm = (evt) => {
@@ -23,52 +49,30 @@ function App() {
       .then((res) => res.json())
       .then((data) => {
         if (data) {
-          setCountry({ ...country, loading: false, data: data, err: "" });
+          setCountry({ loading: false, data: data, err: "" });
         }
       })
-      .catch((err) => {
-        if (err) {
-          setCountry({
-            ...country,
-            loading: false,
-            data: [],
-            err: err.messages,
-          });
-        }
-      });
+      .catch((err) => console.log(err));
 
     inputValue.current.value = "";
   };
-
-  useEffect(() => {
-    setCountry({ ...country, loading: true, data: [], err: "" });
-    fetch("https://restcountries.com/v3.1/all")
-      .then((res) => res.json())
-      .then((data) => {
-        if (data) {
-          setCountry({ ...country, loading: false, data: data, err: "" });
-        }
-      })
-      .catch((err) => {
-        if (err) {
-          setCountry({
-            ...country,
-            loading: false,
-            data: [],
-            err: err.messages,
-          });
-        }
-      });
-  }, []);
 
   return (
     <div className="App">
       <Header />
       <div className="container">
+        {country.loading ? (
+          <img className="loading" src={loading} alt="" />
+        ) : (
+          ""
+        )}
+        {country.err ? <img className="loading" src={error} alt="" /> : ""}
+
         <div className="mt-5 row d-flex justify-content-between">
           <form onSubmit={changeForm} className="col-md-6">
             <div className="input-group input__group">
               <input
+                required
                 ref={inputValue}
                 className="input ms-3"
                 type="text"
@@ -81,18 +85,23 @@ function App() {
           </form>
 
           <div className="col-md-3">
-            <select className="form-select">
-              <option>Open this select menu</option>
-              <option value="1">One</option>
-              <option value="2">Two</option>
-              <option value="3">Three</option>
+            <select onChange={changeSelect} className="form-select">
+              <option disabled>Open this select menu</option>
+              <option value="europe">Europe</option>
+              <option value="antarctic">Antarctic</option>
+              <option value="oceania">Oceania</option>
+              <option value="asia">Asia</option>
+              <option value="africa">Africa</option>
+              <option value="americas">Americas</option>
             </select>
           </div>
         </div>
         <ul className="row gy-4 mt-4 d-flex list-unstyled">
-          {country.data.map((item) => (
-            <Item key={item.name.common} obj={item} />
-          ))}
+          {country.data.length
+            ? country.data.map((item) => (
+                <Item key={item.name.common} obj={item} />
+              ))
+            : <img className="loading1" src={error}/>}
         </ul>
       </div>
     </div>
